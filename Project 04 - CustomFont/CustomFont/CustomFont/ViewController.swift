d3923726c501b1a0ff39c6fa69adc2d9db836cb7
woongs <ccw1021@naver.com>
2019-05-10 01:30:08 +0900

    make CustomFont

--- Project 04 - CustomFont/CustomFont/CustomFont/ViewController.swift
+++ Project 04 - CustomFont/CustomFont/CustomFont/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let customTextField: UITextField = {
-        let textField = UITextField()
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.backgroundColor = UIColor.red
-        return textField
+    let fonts = ["kefa", "papyrus", "zapfino", "bradleyHand"]
+    
+    let customTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.layer.borderWidth = 1.0
+        textView.layer.borderColor = UIColor.black.cgColor
+        textView.textAlignment = .center
+    
+//        textView.isEditable = true
+        
+        
+        return textView
     }()
     
     let changeButton: UIButton = {
@@ -34,29 +42,62 @@ class ViewController: UIViewController {
         navigationItem.title = "Custom Font"
         view.backgroundColor = UIColor.white
         
-        
-        view.addSubview(customTextField)
+        view.addSubview(customTextView)
         view.addSubview(changeButton)
         
-        customTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        customTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
-        customTextField.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
-        customTextField.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        customTextField.bottomAnchor.constraint(equalTo: changeButton.topAnchor, constant: -50).isActive = true
+        [
+            customTextView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            customTextView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+//            customTextView.heightAnchor.constraint(equalToConstant: 50),
+//            customTextView.heightAnchor.constraint(greaterThanOrEqualToConstant: 100),
+            
+//            customTextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            customTextView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 50),
+            customTextView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -50),
+            
+            changeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            changeButton.heightAnchor.constraint(equalToConstant: 50),
+            changeButton.topAnchor.constraint(equalTo: customTextView.bottomAnchor, constant: 50)
+            
+        ].forEach{ ($0).isActive = true }
         
-        changeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        changeButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-//        changeButton.topAnchor.constraint(equalTo: customTextField.bottomAnchor, constant: 50).isActive = true
-        changeButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50).isActive = true
-        
+        customTextView.delegate = self
+        customTextView.isScrollEnabled = false
         
     }
     
     @objc
     func changeButtonTapped(sender: UIButton) {
         print("Font changed")
+        
+        if let font = fonts.randomElement() {
+            customTextView.font = UIFont.init(name: font, size: 25)
+            let size = CGSize(width: customTextView.frame.width, height: .infinity)
+            let estimatedSize = customTextView.sizeThatFits(size)
+            
+            customTextView.constraints.forEach { (constraint) in
+                if constraint.firstAttribute == .height {
+                    constraint.constant = estimatedSize.height
+                }
+            }
+        }
+        
     }
 
 
 }
 
+extension UIViewController: UITextViewDelegate {
+    
+    public func textViewDidChange(_ textView: UITextView) {
+        let size = CGSize(width: textView.frame.width, height: .infinity)
+        let estimatedSize = textView.sizeThatFits(size)
+        
+        textView.constraints.forEach { (constraint) in
+            if constraint.firstAttribute == .height {
+                constraint.constant = estimatedSize.height
+            }
+        }
+    }
+}
