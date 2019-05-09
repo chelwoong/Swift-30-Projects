ddae5c46c41c6d668946d8aab03a45e175894ac4
woongs <ccw1021@naver.com>
2019-05-10 03:09:23 +0900

    first commit, add tableView

---
+++ Project 05 - PlayLocalVideo/PlayLocalVideo/PlayLocalVideo/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  PlayLocalVideo
+//
+//  Created by woong on 10/05/2019.
+//  Copyright © 2019 woong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let videoTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = UIColor.red
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        
+        videoTableView.dataSource = self
+        videoTableView.delegate = self
+        
+        videoTableView.register(UITableViewCell.self, forCellReuseIdentifier: "videoCell")
+        
+        view.addSubview(videoTableView)
+        
+        NSLayoutConstraint.activate([
+            videoTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            videoTableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 0),
+            videoTableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 0),
+            videoTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+            
+            
+            ])
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "videoCell", for: indexPath)
+        
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
+
