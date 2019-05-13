7f1675b32ff71e0ae8321555966259f6b6bdedf3
woongs <ccw1021@naver.com>
2019-05-13 11:44:04 +0900

    first commit

---
+++ Project 07 - FindMyLocation/FindMyLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindMyLocation
+//
+//  Created by woong on 13/05/2019.
+//  Copyright © 2019 woong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+    }
+
+
+}
+
