ed41d8b25a1374782eeda8f86733e90de12366a6
woongs <ccw1021@naver.com>
2019-05-14 22:18:32 +0900

    firt commit

--- Project 07 - FindMyLocation/FindMyLocation/ViewController.swift
+++ Project 07 - FindMyLocation/FindMyLocation/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    var locationManager = CLLocationManager()
+    let mapView: MKMapView = {
+        let map = MKMapView()
+        map.translatesAutoresizingMaskIntoConstraints = false
+        
+        return map
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
+        
+//        view.addSubview(mapView)
+//
+//        NSLayoutConstraint.activate([
+//            mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+//            mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+//            mapView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 0),
+//            mapView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 0)
+//            ])
     }
 
 
