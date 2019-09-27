678b8bde1d3b9dceaaede69cf33ac5e05c15bdbe
woongs <ccw1021@naver.com>
2019-09-27 10:22:29 +0900

    add animation to MainTableView

--- Project 12 - Animations/Animations/MainViewController.swift
+++ Project 12 - Animations/Animations/MainViewController.swift
@@ -11,14 +11,44 @@ import UIKit
 class MainViewController: UIViewController {
     
     let cellId = "cellIdentify"
+    let headerHeight = 50.0
+    let duration = 1.5
+    
     let animations: [Animation] = [.changeColor, .colorFrameChange, .autoChangeColor, .roation2D, .curvePath, .viewFadeIn, ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        animateTable()
         setupNavigation()
         setupConfiguration()
         setupViews()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        animateTable()
+    }
+    
+    func animateTable() {
+        mainTV.reloadData()
+        
+        let cells = mainTV.visibleCells
+        let tableHeight = mainTV.bounds.size.height
+        
+        // move all cells to the bottom of the screen
+        for cell in cells {
+            cell.transform = CGAffineTransform(translationX: 0, y: tableHeight)
+        }
+        
+        // move all cells from bottom to the right place
+        var index = 0
+        for cell in cells {
+            UIView.animate(withDuration: duration, delay: 0.05 * Double(index), usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: [], animations: {
+                cell.transform = CGAffineTransform(translationX: 0, y: 0)
+            }, completion: nil)
+            index += 1
+        }
     }
     
     func setupNavigation() {
